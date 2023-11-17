5f3a042322905e43fa91003beca6697d67721b52
mahisurrahman <zordanblog009@gmail.com>
2023-11-17 07:06:21 +0600

    added the menu dynamically

--- index.js
+++ index.js
@@ -32,6 +32,11 @@ async function run() {
     //Collections//
     const menuCollection = client.db("bistroDb").collection("menu");
 
+    app.get('/menu', async(req, res)=>{
+        const result = await menuCollection.find().toArray();
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
