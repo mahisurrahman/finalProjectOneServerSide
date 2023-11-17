71176e34e6fef51d20d6471cc4d4d04c954f134b
mahisurrahman <zordanblog009@gmail.com>
2023-11-17 19:15:25 +0600

    Shifting to PC

--- index.js
+++ index.js
@@ -31,9 +31,17 @@ async function run() {
 
     //Collections//
     const menuCollection = client.db("bistroDb").collection("menu");
+    const reviewCollection = client.db("bistroDb").collection("reviews");
+
+
 
     app.get('/menu', async(req, res)=>{
         const result = await menuCollection.find().toArray();
+        res.send(result);
+    })
+
+    app.get('/review', async(req, res)=>{
+        const result = await reviewCollection.find().toArray();
         res.send(result);
     })
 
